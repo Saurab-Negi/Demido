0c5a52fd0f0727f2ebb8f6b4815ec28e26ea487d
Saurab-Negi <saurabnegi17@gmail.com>
2024-06-17 12:41:34 +0530

    Responsive s-8 + s-7

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ $(document).ready(function(){
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 2,
             slidesToScroll: 1
           }
         }
